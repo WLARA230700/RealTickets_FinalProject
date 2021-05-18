0c57e9925b629b516663b149612240ba80b66852
WLARA230700 <willy.lara@ucr.ac.cr>
2021-05-17 19:43:09 -0600

    Events Page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let menuMobileOpen = false;
+let searchOpen = false;
+categoriasOpen = false;
 
 function openMenu() {
     let menu = document.getElementsByClassName ("menu-mobile")[0];
@@ -17,6 +19,41 @@ function openMenu() {
         doc.style.overflow = "auto";
     }    
 }
+function openSearch() {
+  let search = document.getElementsByClassName ("search-mobile")[0];
+  let searchbar = document.getElementsByClassName ("searchbar")[0];
+
+  if (!searchOpen) { //ABRIR PANEL
+    search.style.color = "#FFA300";
+    searchOpen = true;
+    searchbar.style.opacity = "100%";
+  }else{ //CERRAR PANEL
+    search.style.color = "#F0F0F0";
+    searchOpen = false;
+    searchbar.style.opacity = "0%";
+  }    
+}
+function openCategorias() {
+  let bgCategorias = document.getElementsByClassName("eventos-categorias-bg")[0];
+  let textCategorias = document.getElementsByClassName("eventos-categorias-text")[0];
+  let masCategorias = document.getElementsByClassName("float-right")[0];
+  let categoriasOpciones = document.getElementsByClassName ("eventos-categorias-list")[0];
+
+  if (!categoriasOpen) { //ABRIR PANEL
+    bgCategorias.style.margin = "1em auto 0";
+    textCategorias.style.color = "#FFA300";
+    masCategorias.style.color = "#FFA300";
+    categoriasOpen = true;
+    categoriasOpciones.style.display = "block";
+  }else{ //CERRAR PANEL
+    bgCategorias.style.margin = "1em auto";
+    textCategorias.style.color = "#F0F0F0";
+    masCategorias.style.color = "#F0F0F0";
+    categoriasOpen = false;
+    categoriasOpciones.style.display = "none";
+  }    
+}
+
 
 window.onscroll = function() {scrollFunction()};
 
