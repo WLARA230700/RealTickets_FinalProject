af82c8ad80ba1bac59923a2167c518a368844547
WLARA230700 <willy.lara@ucr.ac.cr>
2021-05-29 20:13:50 -0600

    Index correction

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
-    <link rel="preload" href="./css/style.css" as="style">
+    <link rel="stylesheet" href="./css/style.css">
 
     <title>RealTickets</title>
 </head>
