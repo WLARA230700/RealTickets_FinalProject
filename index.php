ccacef3240352a788782039f456d93d00b40c6d8
WLARA230700 <willy.lara@ucr.ac.cr>
2021-05-29 20:12:12 -0600

    Imgs modifications

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Página de inicio del sitio web RealTickets, encargado de la venta de tiquetes de distintos eventos.">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preload" href="./css/style.css" as="style">
 
     <title>RealTickets</title>
 </head>
